2e847aa85c8df0f39bd978c63125295fd1121ae1
Junhuai <850217160@qq.com>
2018-06-19 15:36:05 +0800

    +php

---
+++ http/submit.php
@@ -0,0 +1,161 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Auto Club</title>
+<link href="css/master.css" rel="stylesheet">
+
+<!-- SWITCHER -->
+<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color1.css" title="color1" media="all" data-default-color="true" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color2.css" title="color2" media="all" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color3.css" title="color3" media="all" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color4.css" title="color4" media="all" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color5.css" title="color5" media="all" />
+<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color6.css" title="color6" media="all" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/flat-ui.min.css" />
+<link rel="stylesheet" type="text/css" href="css/jquery.nouislider.css">
+<link rel="stylesheet" type="text/css" href="css/common.css" />
+<link rel="stylesheet" type="text/css" href="css/slide.css" />
+
+<!--[if lt IE 9]>
+		<script src="/oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="/oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+<body class="m-submit1" data-scrolling-animations="true">
+
+<!-- Loader -->
+<div id="page-preloader"><span class="spinner"></span></div>
+<!-- Loader end --> 
+<!--b-topBar-->
+<header class="b-nav">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3 col-xs-4">
+        <div class="b-nav__logo wow slideInLeft" data-wow-delay="0.3s">
+          <h3><a href="submit.html">格里芬<span>租车</span></a></h3>
+          <h2><a href="submit.html">租车新体验</a></h2>
+        </div>
+      </div>
+      <div class="col-sm-9 col-xs-8">
+        <div class="b-nav__list wow slideInRight" data-wow-delay="0.3s">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          </div>
+          <div class="collapse navbar-collapse navbar-main-slide" id="nav">
+            <ul class="navbar-nav-menu">
+              <li><a href="submit.html">首页</a></li>
+              <li><a href="submit.html">订单查询</a></li>
+              <li><a href="contacts.html">退出登录</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!--b-nav-->
+
+<section class="b-pageHeader">
+  <div class="container">
+    <h1 class="wow zoomInLeft" data-wow-delay="0.5s">提交订单</h1>
+  </div>
+</section>
+<!--b-pageHeader-->
+
+<div class="b-breadCumbs s-shadow">
+  <div class="container wow zoomInUp" data-wow-delay="0.5s"> <a href="submit.html" class="b-breadCumbs__page">首页</a><span class="fa fa-angle-right"></span><a href="submit1.html" class="b-breadCumbs__page m-active">提交订单</a> </div>
+</div>
+<!--b-breadCumbs--> 
+<!--b-infoBar-->
+
+<div class="b-submit">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-9 col-md-8 col-sm-7 col-xs-6">
+        <div class="b-submit__main">
+          <header class="s-headerSubmit s-lineDownLeft wow zoomInUp" data-wow-delay="0.5s">
+            <h2 class="">订单详情</h2>
+          </header>
+          <form class="s-submit clearfix" action="submit2.html" method="POST">
+            <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <div class="b-submit__main-element wow zoomInUp" data-wow-delay="0.5s">
+                  <label>车辆编号 <span>*</span></label>
+                  <div class='s-relative'>
+                    <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+                     </div>
+                </div>
+<div class="b-submit__main-element wow zoomInUp" data-wow-delay="0.5s">
+                  <label>预定天数 <span>*</span></label>
+                  <div class='s-relative'>
+                    <input type="text" class="form-control" placeholder="预定几天" aria-describedby="basic-addon1">
+                     </div>
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn m-btn pull-right wow zoomInUp" data-wow-delay="0.5s">提 交 订 单 ！<span class="fa fa-angle-right"></span></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--b-submit-->
+
+<div class="m-home"> <!--b-info-->
+  <footer class="b-footer"> <a id="to-top2" href="#this-is-top"><i class="fa fa-chevron-up"></i></a>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-4">
+          <div class="b-footer__company wow fadeInLeft" data-wow-delay="0.3s">
+            <div class="b-nav__logo">
+              <h3><a href="submit.html">格里芬<span>租车</span></a></h3>
+            </div>
+            <p>Copyright &copy; 2018.YangJunhuai All rights reserved.</p>
+          </div>
+        </div>
+		  <div class="col-xs-8">
+			<div class="b-footer__content wow zoomInRight" data-wow-delay="0.5s">
+								<div class="b-footer__content-social">
+									<a href="#https://github.com/JunhuaiYang"><span class="fa fa-github"></span></a>
+								</div>
+</div>
+						</div>
+        <div class="col-xs-8"></div>
+      </div>
+    </div>
+  </footer>
+  <!--b-footer--> 
+  
+</div>
+<!--Main--> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<script src="js/jquery-ui.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/modernizr.custom.js"></script> 
+<script src="assets/rendro-easy-pie-chart/dist/jquery.easypiechart.min.js"></script> 
+<script src="js/waypoints.min.js"></script> 
+<script src="js/jquery.easypiechart.min.js"></script> 
+<script src="js/classie.js"></script> 
+
+<!--Switcher--> 
+<script src="assets/switcher/js/switcher.js"></script> 
+<!--Owl Carousel--> 
+<script src="assets/owl-carousel/owl.carousel.min.js"></script> 
+<!--bxSlider--> 
+<script src="assets/bxslider/jquery.bxslider.js"></script> 
+<!-- jQuery UI Slider --> 
+<script src="assets/slider/jquery.ui-slider.js"></script> 
+
+<!--Theme--> 
+<script src="js/jquery.smooth-scroll.js"></script> 
+<script src="js/wow.min.js"></script> 
+<script src="js/jquery.placeholder.min.js"></script> 
+<script src="js/theme.js"></script>
+</body>
+</html>
